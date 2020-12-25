b1490bf6e3c41fea10680ffede1ee5502435b6f9
quangtuyen99 <59727231+quangtuyen99@users.noreply.github.com>
2020-12-25 11:15:29 +0700

    v1

--- app/src/main/java/com/example/medicine/SQLConnect.java
+++ app/src/main/java/com/example/medicine/SQLConnect.java
@@ -16,7 +16,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class SQLConnect {
-    private static String ip = "172.16.0.148"; // cmd + ipconfig
+    private static String ip = "192.168.0.106"; // cmd + ipconfig
     private static String port = "1433";
     private static String Classes = "net.sourceforge.jtds.jdbc.Driver";
     private static String database = "QLThuoc"; // Tên database
